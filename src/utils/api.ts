1cf9c6436447702624321cab45ccd93e082122dd
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-04 23:14:11 +0800

    refactor: api refactor

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,20 @@
 import fetch from "node-fetch";
 import { getPreferenceValues } from "@raycast/api";
-import { Preferences, MinifluxApiError, MinifluxEntries, MinifluxEntry, IconData, originArticle } from "./types";
+import { Preferences, MinifluxApiError, MinifluxEntries, MinifluxEntry, IconData, OriginArticle } from "./types";
 
-const removeTrailingSlash = (baseUrl: string): string =>
-  baseUrl.charAt(baseUrl.length - 1) === "/" ? baseUrl.slice(0, -1) : baseUrl;
+const removeTrailingSlash = (baseUrl: string): string => baseUrl.endsWith("/") ? baseUrl.slice(0, -1) : baseUrl
 
-const fetchData = async <T>(endpoint: string, queryParams?: string): Promise<T> => {
-  const preferences: Preferences = getPreferenceValues();
-  const { baseUrl, apiKey } = preferences;
+const requestApi = async <T>(
+  endpoint: string,
+  queryParams?: string,
+  method: "GET" | "POST" | "PUT" = "GET"
+): Promise<T> => {
+  const { baseUrl, apiKey } = getPreferenceValues<Preferences>();
   const apiUrl = removeTrailingSlash(baseUrl);
 
-  const response = await fetch(apiUrl + endpoint + (queryParams || ""), {
-    method: "get",
-    headers: {
-      "X-Auth-Token": apiKey,
-    },
+  const response = await fetch(apiUrl + endpoint + ( queryParams || ""), {
+    method,
+    headers: { "X-Auth-Token": apiKey },
   });
 
   if (!response.ok) {
@@ -24,62 +24,32 @@ const fetchData = async <T>(endpoint: string, queryParams?: string): Promise<T> 
   return (await response.json()) as T;
 };
 
-export const fetchEntriesWithParams = async <T>(queryParams: string): Promise<T> => {
-  return await fetchData<T>("/v1/entries", queryParams);
-};
+export const fetchEntriesWithParams = async <T>(queryParams: string): Promise<T> =>
+  requestApi<T>("/v1/entries", queryParams);
 
 export const search = async (query: string): Promise<MinifluxEntries> => {
-  const preferences: Preferences = getPreferenceValues();
-  const { searchLimit } = preferences;
-
-  return await fetchEntriesWithParams(`?search=${query}${searchLimit ? "&limit=" + searchLimit : ""}`);
+  const { searchLimit } = getPreferenceValues<Preferences>();
+  return fetchEntriesWithParams<MinifluxEntries>(`?search=${query}${searchLimit ? "&limit=" + searchLimit : ""}`);
 };
 
 export const getRecentEntries = async (): Promise<MinifluxEntries> => {
-  const preferences: Preferences = getPreferenceValues();
-  const { feedLimit } = preferences;
-
-  return await fetchEntriesWithParams(`?status=unread&direction=desc&limit=${feedLimit}`);
+  const { feedLimit } = getPreferenceValues<Preferences>();
+  return fetchEntriesWithParams<MinifluxEntries>(`?status=unread&direction=desc&limit=${feedLimit}`);
 };
 
 export const getEntryUrlInMiniflux = ({ id, status }: MinifluxEntry): string => {
-  const preferences: Preferences = getPreferenceValues();
-  const { baseUrl } = preferences;
+  const { baseUrl } = getPreferenceValues<Preferences>();
   const entryStatus = status === "read" ? "history" : status;
 
   return `${baseUrl}/${entryStatus}/entry/${id}`;
 };
 
-export const fetchIconForFeed = async ({ feed_id }: MinifluxEntry): Promise<IconData> => {
-  return await fetchData<IconData>(`/v1/feeds/${feed_id}/icon`);
-};
+export const fetchIconForFeed = async ({ feed_id }: MinifluxEntry): Promise<IconData> =>
+   requestApi<IconData>(`/v1/feeds/${feed_id}/icon`);
 
-export const fetchOriginArticle = async ({ id }: MinifluxEntry): Promise<originArticle> => {
-  return await fetchData(`/v1/entries/${id}/fetch-content`);
-};
+export const fetchOriginArticle = async ({ id }: MinifluxEntry): Promise<OriginArticle> =>
+  requestApi<OriginArticle>(`/v1/entries/${id}/fetch-content`);
 
-// /v1/entries/1234/bookmark
-const updateData = async <T>(endpoint: string, method: "post" | "put"): Promise<T> => {
-  const preferences: Preferences = getPreferenceValues();
-  const { baseUrl, apiKey } = preferences;
-  const apiUrl = removeTrailingSlash(baseUrl);
 
-  const response = await fetch(apiUrl + endpoint, {
-    method,
-    headers: {
-      "X-Auth-Token": apiKey,
-    },
-  });
-
-  if (!response.ok) {
-    throw (await response.json()) as MinifluxApiError;
-  }
-
-  return response.status as T;
-};
-
-export const toggleBookmark = async ({ id }: MinifluxEntry): Promise<boolean> => {
-  const statusCode = await updateData<number>(`/v1/entries/${id}/bookmark`, "put");
-
-  return statusCode === 204;
-};
+export const toggleBookmark = async ({ id }: MinifluxEntry): Promise<boolean> =>
+  (await requestApi<number>(`/v1/entries/${id}/bookmark`, "","PUT")) === 204;
