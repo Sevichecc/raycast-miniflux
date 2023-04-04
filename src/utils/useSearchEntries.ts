fcf59ca17592f8dba771fb3e292a3d7d0732f927
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-04 19:28:53 +0800

    refactor: load latest feed before search

--- src/utils/useSearchEntries.ts
+++ src/utils/useSearchEntries.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import { MinifluxApiError, MinifluxEntry } from "./types";
-import { search } from "./apis";
+import { showToast, Toast, popToRoot } from "@raycast/api";
+import { useCallback, useEffect, useState } from "react";
+import { MinifluxApiError, MinifluxEntry, MinifluxEntries } from "./types";
+import { search, getRecentEntries } from "./apis";
 
 interface State {
   isLoading: boolean;
@@ -11,38 +12,29 @@ interface State {
 export const useSearchEntries = (searchText: string) => {
   const [state, setState] = useState<State>({ isLoading: false });
 
-  useEffect(() => {
-    if (!searchText) {
-      setState({ isLoading: false });
-      return;
-    }
+  const fetchData = useCallback(async () => {
+    setState((oldState) => ({ ...oldState, isLoading: true }));
 
-    let isMounted = true;
-
-    const fetchData = async () => {
-      setState((oldState) => ({ ...oldState, isLoading: true }));
-
-      try {
-        const { entries } = await search(searchText);
-
-        if (isMounted) {
-          setState((oldState) => ({ ...oldState, entries }));
-        }
-      } catch (error) {
-        console.log(error as MinifluxApiError);
-      } finally {
-        if (isMounted) {
-          setState((oldState) => ({ ...oldState, isLoading: false }));
-        }
+    try {
+      const entries: MinifluxEntries = searchText ? await search(searchText) : await getRecentEntries();
+      setState({ ...entries, isLoading: false });
+    } catch (error) {
+      const apiError = error as MinifluxApiError;
+      let errorMessage = "Failed to load feeds";
+      
+      if (apiError?.code === "401") {
+        errorMessage = "Invalid Credentials. Check your API key and try again.";
+        popToRoot({ clearSearchBar: true });
       }
-    };
 
-    fetchData();
-
-    return () => {
-      isMounted = false;
-    };
+      showToast(Toast.Style.Failure, errorMessage);
+      setState((oldState) => ({ ...oldState, isLoading: false, error: apiError }));
+    }
   }, [searchText]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return state;
 };
