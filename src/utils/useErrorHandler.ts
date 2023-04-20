535f2a48f69864d9894093dcafa8cb3fa97a6ec1
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-20 22:40:17 +0800

    feat: add kaomoji

--- src/utils/useErrorHandler.ts
+++ src/utils/useErrorHandler.ts
@@ -1,4 +1,4 @@
-import { popToRoot,showToast,Toast} from "@raycast/api";
+import { popToRoot, showToast, Toast } from "@raycast/api";
 import { MinifluxApiError } from "./types";
 import { useCallback } from "react";
 
@@ -7,7 +7,7 @@ export const useErrorHandler = () => {
     let errorMessage = error.error_message || "Failed to load feeds";
 
     if (error?.code === "401") {
-      errorMessage = "Invalid Credentials. Check your API key and try again.";
+      errorMessage = "Invalid Credentials. Check your API key and try again. (ToT)";
       popToRoot({ clearSearchBar: true });
     }
 
